2969ce656eb9fec56bbf27034fcfabccf58918b8
EnterOrion <orionwesson@gmail.com>
2023-03-03 20:48:56 +0100

    Style welcome instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,9 +1,12 @@
 import React from "react";
+import arrowIcon from "../images/flecha.png";
 
 const Instructions = () => {
     return (
-        <div>
-            <p>Welcome to your resume builder! <br/> Fill out each section and then click the button to add it to your resume below. 
+        <div className="welcome-text">
+            <h1>Looking for an effortless way to design an resume?</h1>
+            <h3><img alt="" src={arrowIcon}></img>  Welcome to <span class="blue-text">your personal resume builder.</span> </h3>
+            <p className="welcome-para">  Fill out each section and then click the button to add it to your resume below. 
                 If you want to edit any information, just click the text on the resume that you want to change and click "update" when you're done. You can also delete sections entirely.
             </p>
         </div>
