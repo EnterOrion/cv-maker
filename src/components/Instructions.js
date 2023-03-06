b36b9a2b4783232006a823bc073bcad0af760564
EnterOrion <orionwesson@gmail.com>
2023-03-06 22:08:01 +0100

    Print media query

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -7,7 +7,9 @@ const Instructions = () => {
             <h1>Looking for an effortless way to design an resume?</h1>
             <h3><img alt="" src={arrowIcon}></img>  Welcome to <span class="blue-text">your personal resume builder.</span> </h3>
             <p className="welcome-para">  <strong>Fill out each section</strong> and then click the button next to it to add the section to your resume. 
-               You can also <strong>click on text</strong> on the resume itself if you want to edit any individual pieces of information and then <strong>click "update"</strong> when you're done. You can also delete sections entirely.
+               You can also <strong>click on text</strong> on the resume itself if you want to edit any individual pieces of information and then <strong>click "update"</strong> when you're done. You can also delete sections entirely. <br/>
+               When you're finished, press <strong>ctrl + p</strong> to print!
+
             </p>
         </div>
     )
